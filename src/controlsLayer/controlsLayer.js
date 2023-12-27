0b98683d62b674c6eda8e5aa24978ad0c6dd47b5
Diego Escobar <diego.escobar@pixlr.com>
2023-12-28 01:42:44 +0200

    TESTING POLYLINE

--- src/controlsLayer/controlsLayer.js
+++ src/controlsLayer/controlsLayer.js
@@ -1,4 +1,122 @@
+import "./controlsLayer.scss";
+import { viewer } from "../mapLayer";
 
-import { NavigationHelpButton } from "cesium"
-import "./controlsLayer.scss"
+const MINIMUM_CAMERA_HEIGHT = 100;
+const MAXIMUM_CAMERA_HEIGHT = 100_000_000;
+const zoomInElement = document.querySelector("#zoom-in");
+const zoomOutElement = document.querySelector("#zoom-out");
+const heightIndicatorElement = document.querySelector("#height-indicator");
 
+const isTooFar = () => {
+  const cameraHeightMeters = viewer.camera.positionCartographic.height;
+  return cameraHeightMeters >= MAXIMUM_CAMERA_HEIGHT;
+};
+
+const isTooClose = () => {
+  const cameraHeightMeters = viewer.camera.positionCartographic.height;
+  return cameraHeightMeters <= MINIMUM_CAMERA_HEIGHT;
+};
+
+const onCameraChanged = () => {
+  const cameraHeightMeters = viewer.camera.positionCartographic.height;
+
+  heightIndicatorElement.innerText =
+    (cameraHeightMeters / 1000).toFixed(2) + " km";
+
+  if (isTooClose()) {
+    zoomInElement.disabled = true;
+  } else {
+    zoomInElement.disabled = false;
+  }
+
+  if (isTooFar()) {
+    zoomOutElement.disabled = true;
+  } else {
+    zoomOutElement.disabled = false;
+  }
+};
+
+onCameraChanged();
+viewer.camera.changed.addEventListener(onCameraChanged);
+
+// zoomInElement.addEventListener("click", () => {
+//   zoomAction(true);
+// });
+
+// zoomOutElement.addEventListener("click", () => {
+//   zoomAction(false);
+// });
+
+let zoomInterval;
+let isHolding = false;
+
+const zoomAction = (zoomIn) => {
+  const height = viewer.camera.positionCartographic.height;
+  const zoomValue = 0.2 * height;
+  if (zoomIn) {
+    if (!isTooClose()) {
+      viewer.camera.zoomIn(zoomValue);
+    }
+  } else if (!isTooFar()) {
+    viewer.camera.zoomOut(zoomValue);
+  }
+};
+
+const startZoom = (zoomIn) => {
+  if (zoomInterval) clearInterval(zoomInterval);
+  zoomInterval = setInterval(() => {
+    zoomAction(zoomIn);
+  }, 100); // Adjust the interval for smoother or faster zoom
+};
+
+const stopZoom = () => {
+  isHolding = false;
+  clearInterval(zoomInterval);
+  zoomInterval = null;
+};
+
+const handleZoom = (zoomIn) => {
+  zoomAction(zoomIn);
+
+  setTimeout(() => {
+    if (isHolding) {
+      startZoom(zoomIn);
+    }
+  }, 500);
+};
+
+zoomInElement.addEventListener("mousedown", () => {
+  isHolding = true;
+  handleZoom(true);
+});
+
+zoomInElement.addEventListener(
+  "touchstart",
+  () => {
+    isHolding = true;
+    handleZoom(true);
+  },
+  { passive: true }
+);
+
+zoomOutElement.addEventListener("mousedown", () => {
+  isHolding = true;
+  handleZoom(false);
+});
+
+zoomOutElement.addEventListener(
+  "touchstart",
+  () => {
+    isHolding = true;
+    handleZoom(false);
+  },
+  { passive: true }
+);
+
+zoomInElement.addEventListener("mouseup", stopZoom);
+zoomInElement.addEventListener("mouseleave", stopZoom);
+zoomInElement.addEventListener("touchend", stopZoom);
+
+zoomOutElement.addEventListener("mouseup", stopZoom);
+zoomOutElement.addEventListener("mouseleave", stopZoom);
+zoomOutElement.addEventListener("touchend", stopZoom);
--- src/mapLayer/index.js
+++ src/mapLayer/index.js
@@ -9,13 +9,15 @@ import {
   HeadingPitchRange,
   Transforms,
   JulianDate,
+  Color,
+  PolylineOutlineMaterialProperty,
 } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
 Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_TOKEN;
 
 // Initialize the Cesium Viewer in the HTML element with the `app` ID.
-const viewer = new Viewer("app", {
+export const viewer = new Viewer("app", {
   terrain: Terrain.fromWorldTerrain(),
   infoBox: false,
   selectionIndicator: false,
@@ -30,17 +32,64 @@ const viewer = new Viewer("app", {
   scene3DOnly: true,
 });
 
+viewer.camera.percentageChanged = 0.001;
+
 viewer.camera.flyTo({
-  destination: Cartesian3.fromDegrees(24.7536, 59.437, 1_000),
+  destination: Cartesian3.fromDegrees(24.7536, 59.437, 100_000),
   orientation: {
     heading: CesiumMath.toRadians(0.0),
-    pitch: CesiumMath.toRadians(-45.0),
+    pitch: CesiumMath.toRadians(-90.0),
   },
   duration: 0,
-  //   complete: function () {
-  //     followPath();
-  //   },
 });
+
+
+const dot = viewer.entities.add({
+  position: Cartesian3.fromDegrees(24.7536, 59.437, 1_000),
+  point: {
+    pixelSize: 20,
+    color: Color.RED,
+  },
+});
+
+
+const orangeOutlined = viewer.entities.add({
+  name:
+    "Orange line with black outline at height and following the surface",
+  polyline: {
+    positions: Cartesian3.fromDegreesArrayHeights([
+      -75,
+      39,
+      250000,
+      -125,
+      39,
+      250000,
+    ]),
+    width: 25,
+    material: new PolylineOutlineMaterialProperty({
+      color: Color.ORANGE,
+      outlineWidth: 10,
+      outlineColor: Color.BLACK,
+    }),
+  },
+});
+
+viewer.zoomTo(orangeOutlined);
+
+
+
+viewer.selectedEntityChanged.addEventListener((entity) => {
+  console.log(entity);
+  viewer.trackedEntity = entity;
+  if(!entity) {
+    
+    viewer.entities.remove(orangeOutlined);
+    return;}
+  viewer.flyTo(entity);
+
+
+});
+
 
 
 
@@ -77,10 +126,13 @@ const followPath = async () => {
 //   new HeadingPitchRange(0, -Math.PI / 8, 2900)
 // );
 
-
-
+// const initialTimer = Date.now();
 
 // viewer.clock.onTick.addEventListener(function (clock) {
-// //   console.log(Date.now())
+
+//   const initialX = 24.7536;
+//   const currentX = (Date.now() - initialTimer) / 100000  + initialX;
+  
+//   dot.position = Cartesian3.fromDegrees(currentX, 59.437, 1_000);
 // });
   
